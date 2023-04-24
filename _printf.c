4227eb66d14d5b47d4e8de798e048d28655288be
ioulasri <imad.oulasri@iu-study.org>
2023-04-24 23:48:19 +0100

    ok

---
+++ _printf.c
@@ -0,0 +1,82 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int _putchar(char c)
+{
+    return (write(1, &c, 1));
+}
+
+int handle_string(char *str)
+{
+    int i = 0;
+
+    if (str == NULL)
+    {
+        handle_string("(null)");
+        return (6);
+    }
+    while (str[i])
+    {
+        putchar(str[i]);
+        i++;
+    }
+    return (i);
+}
+
+int _printf(const char *format, ...)
+{
+    int count = 0;
+
+    va_list args;
+    va_start(args, format);
+
+    if (!format || !format[0])
+        return (-1);
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            if (*format == 'c') 
+            {
+                char c = va_arg(args, int);
+                count += _putchar(c);
+                
+            }
+            else if (*format == 's')
+            {
+                char *str = va_arg(args, char *);
+                count += handle_string(str);
+            }
+            else if (*format == '%')
+            {
+                _putchar('%');
+                count++;
+            }
+            else 
+            {
+                _putchar('%');
+                count++;
+                if (*format)
+                {
+                  _putchar(*format);   
+                  count++;
+                }
+            }
+            format++;
+        }
+        else
+        {
+            _putchar(*format);
+            format++;
+            count++;
+        }
+    }
+    return (count);
+}
+
+int main()
+{
+  _printf("hello %s %r %c %% %", "world", '\n');
+}
